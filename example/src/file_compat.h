8bc53962861b68ae4e8d6bbe7d420d0a8d0b7f40
David Brackeen <brackeen@gmail.com>
2019-06-04 15:36:13 -0700

    Update file_compat

--- example/src/file_compat.h
+++ example/src/file_compat.h
@@ -1,7 +1,7 @@
 /*
  file-compat
  https://github.com/brackeen/file-compat
- Copyright (c) 2017 David Brackeen
+ Copyright (c) 2017-2019 David Brackeen
 
  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -23,24 +23,26 @@
 #define FILE_COMPAT_H
 
 /**
-    Redefines common `stdio` functions so that they work as expected on Windows and Android.
-
-    Additionally, includes the function `fc_resdir() to get the path to the current executable's
-    directory or its resources directory. This function works on Windows, Linux, macOS, iOS, and
-    Android.
+    ## Redefined Functions (Windows, Android):
 
     | Function            | Windows                      | Android
     |---------------------|------------------------------|-----------------------------------------
     | `printf`            | Uses `OutputDebugString`*    | Uses `__android_log_print`
     | `fopen`             | Uses `fopen_s`               | Uses `AAssetManager_open` if read mode
     | `fclose`            | Adds `NULL` check            | No change
-    | `chdir`             | Uses `_chdir`                | No change
-    | `sleep` / `usleep`  | Uses `Sleep`                 | No change
 
     *`OutputDebugString` is only used if the debugger is present and no console is allocated.
     Otherwise uses `printf`.
 
-    ## Usage
+    ## Added Functions (Windows, Linux, macOS, iOS, Android, Emscripten):
+
+    | Function     | Description
+    |--------------|--------------------------------------------------------------------------------
+    | `fc_resdir`  | Gets the path to the current executable's directory
+    | `fc_locale`  | Gets the user's preferred language (For example, "en-US")
+
+    ## Usage:
+
     For Android, define `FILE_COMPAT_ANDROID_ACTIVITY` to be a reference to an `ANativeActivity`
     instance or to a function that returns an `ANativeActivity` instance. May be `NULL`.
 
@@ -50,17 +52,11 @@
 
 #include <stdio.h>
 #include <errno.h>
-#if defined(_WIN32)
-#  include <direct.h>
-#  if !defined(PATH_MAX)
-#    define PATH_MAX 1024
-#  endif
+
+#if defined(__GNUC__)
+#  define FC_UNUSED __attribute__ ((unused))
 #else
-#  include <unistd.h>
-#  include <limits.h> /* PATH_MAX */
-#endif
-#if defined(__APPLE__)
-#  include <CoreFoundation/CoreFoundation.h>
+#  define FC_UNUSED
 #endif
 
 /**
@@ -76,6 +72,59 @@
     @param path_max The length of the buffer. Should be `PATH_MAX`.
     @return 0 on success, -1 on failure.
  */
+static int fc_resdir(char *path, size_t path_max) FC_UNUSED;
+
+/**
+    Gets the preferred user language in BCP-47 format. Valid examples are "en", "en-US",
+    "zh-Hans", and "zh-Hans-HK". Some platforms may return values in lowercase ("en-us" instead of
+    "en-US").
+
+    @param locale The buffer to fill the locale. No more than `locale_max` bytes are writen to the
+    buffer, including the trailing 0. If failure occurs, the locale is set to an empty string.
+    @param locale_max The length of the buffer. This value must be at least 3.
+    @return 0 on success, -1 on failure.
+*/
+static int fc_locale(char *locale, size_t locale_max) FC_UNUSED;
+
+#if defined(_WIN32)
+#  if !defined(WIN32_LEAN_AND_MEAN)
+#    define WIN32_LEAN_AND_MEAN
+#  endif
+#  include <windows.h>
+#  include <stdlib.h> /* wcstombs_s */
+#  if !defined(PATH_MAX)
+#    define PATH_MAX 1024
+#  endif
+#else
+#  include <limits.h> /* PATH_MAX */
+#endif
+#if defined(__APPLE__)
+#  include <CoreFoundation/CoreFoundation.h>
+#  include <objc/objc.h>
+#  include <objc/runtime.h>
+#  include <objc/message.h>
+#  define FC_MSG_SEND ((id (*)(id, SEL))objc_msgSend)
+#  define FC_AUTORELEASEPOOL_BEGIN { \
+       id autoreleasePool = FC_MSG_SEND(FC_MSG_SEND((id)objc_getClass("NSAutoreleasePool"), \
+           sel_registerName("alloc")), sel_registerName("init"));
+#  define FC_AUTORELEASEPOOL_END \
+       FC_MSG_SEND(autoreleasePool, sel_registerName("release")); }
+#elif defined(__EMSCRIPTEN__)
+#  include <emscripten/emscripten.h>
+#  include <string.h>
+#elif defined(__ANDROID__)
+#  include <android/asset_manager.h>
+#  include <android/log.h>
+#  include <android/native_activity.h>
+#  include <jni.h>
+#  include <pthread.h>
+#  include <string.h>
+static JNIEnv *_fc_jnienv(JavaVM *vm);
+#elif defined(__linux__)
+#  include <locale.h>
+#  include <string.h>
+#endif
+
 static int fc_resdir(char *path, size_t path_max) {
     if (!path || path_max == 0) {
         return -1;
@@ -94,7 +143,7 @@ static int fc_resdir(char *path, size_t path_max) {
     return -1;
 #elif defined(__linux__) && !defined(__ANDROID__)
     ssize_t length = readlink("/proc/self/exe", path, path_max - 1);
-    if (length > 0 && length < path_max) {
+    if (length > 0 && (size_t)length < path_max) {
         for (ssize_t i = length - 1; i > 0; i--) {
             if (path[i] == '/') {
                 path[i + 1] = 0;
@@ -105,12 +154,14 @@ static int fc_resdir(char *path, size_t path_max) {
     path[0] = 0;
     return -1;
 #elif defined(__APPLE__)
+    int result = -1;
+    FC_AUTORELEASEPOOL_BEGIN
     CFBundleRef bundle = CFBundleGetMainBundle();
     if (bundle) {
         CFURLRef resourcesURL = CFBundleCopyResourcesDirectoryURL(bundle);
         if (resourcesURL) {
             Boolean success = CFURLGetFileSystemRepresentation(resourcesURL, TRUE, (UInt8 *)path,
-                                                               path_max - 1);
+                                                               (CFIndex)path_max - 1);
             CFRelease(resourcesURL);
             if (success) {
                 unsigned long length = strlen(path);
@@ -120,13 +171,16 @@ static int fc_resdir(char *path, size_t path_max) {
                         path[length] = '/';
                         path[length + 1] = 0;
                     }
-                    return 0;
+                    result = 0;
                 }
             }
         }
     }
-    path[0] = 0;
-    return -1;
+    FC_AUTORELEASEPOOL_END
+    if (result != 0) {
+        path[0] = 0;
+    }
+    return result;
 #elif defined(__ANDROID__)
     path[0] = 0;
     return 0;
@@ -138,24 +192,162 @@ static int fc_resdir(char *path, size_t path_max) {
 #endif
 }
 
+static int fc_locale(char *locale, size_t locale_max) {
+    if (!locale || locale_max < 3) {
+        return -1;
+    }
+    int result = -1;
+#if defined(_WIN32)
+    typedef int (WINAPI *GetUserDefaultLocaleName_t)(LPWSTR, int);
+    GetUserDefaultLocaleName_t pGetUserDefaultLocaleName = NULL;
+
+    // Use GetUserDefaultLocaleName on Windows Vista and newer
+    pGetUserDefaultLocaleName = (GetUserDefaultLocaleName_t)GetProcAddress(
+        GetModuleHandle("kernel32.dll"), "GetUserDefaultLocaleName");
+    if (pGetUserDefaultLocaleName) {
+        wchar_t wlocale[LOCALE_NAME_MAX_LENGTH];
+        if (pGetUserDefaultLocaleName(wlocale, LOCALE_NAME_MAX_LENGTH) > 0) {
+            size_t count = 0;
+            if (wcstombs_s(&count, locale, locale_max, wlocale, locale_max - 1) == 0) {
+                result = 0;
+            }
+        }
+    } else {
+        // Use GetLocaleInfoA on Windows XP
+        int length = GetLocaleInfoA(LOCALE_USER_DEFAULT, LOCALE_SISO639LANGNAME, locale,
+                                    locale_max);
+        if (length > 0) {
+            if (locale_max - length > 0) {
+                int length2 = GetLocaleInfoA(LOCALE_USER_DEFAULT, LOCALE_SISO3166CTRYNAME,
+                                             locale + length, locale_max - length);
+                if (length2 > 0) {
+                    locale[length - 1] = '-';
+                }
+            }
+            result = 0;
+        }
+    }
+#elif defined(__linux__) && !defined(__ANDROID__)
+    setlocale(LC_ALL, "");
+    char *lang = setlocale(LC_ALL, NULL);
+    if (lang && lang[0] != 0 && !(lang[0] == 'C' && lang[1] == 0)) {
+        result = 0;
+        strncpy(locale, lang, locale_max);
+        locale[locale_max - 1] = 0;
+    }
+#elif defined(__APPLE__)
+    FC_AUTORELEASEPOOL_BEGIN
+    CFArrayRef languages = CFLocaleCopyPreferredLanguages();
+    if (languages) {
+        if (CFArrayGetCount(languages) > 0) {
+            CFStringRef language = CFArrayGetValueAtIndex(languages, 0);
+            if (language) {
+                CFIndex length = CFStringGetLength(language);
+                CFIndex outLength = 0;
+                CFStringGetBytes(language, CFRangeMake(0, length), kCFStringEncodingUTF8, 0, FALSE,
+                                 (UInt8 *)locale, (CFIndex)locale_max - 1, &outLength);
+                locale[outLength] = 0;
+                result = 0;
+            }
+        }
+        CFRelease(languages);
+    }
+    FC_AUTORELEASEPOOL_END
+#elif defined(__EMSCRIPTEN__)
+    static const char *script =
+        "(function() { try {"
+        "var lang = navigator.language || navigator.userLanguage || navigator.browserLanguage;"
+        "if (typeof lang === 'string') { return lang; } else { return ''; }"
+        "} catch(err) { return ''; } }())";
+
+    char *lang = emscripten_run_script_string(script);
+    if (lang && lang[0] != 0) {
+        result = 0;
+        strncpy(locale, lang, locale_max);
+        locale[locale_max - 1] = 0;
+    }
+#elif defined(__ANDROID__)
+    ANativeActivity *activity = FILE_COMPAT_ANDROID_ACTIVITY;
+    if (activity) {
+        // getResources().getConfiguration().locale.toString()
+        JNIEnv *jniEnv = _fc_jnienv(activity->vm);
+        if ((*jniEnv)->ExceptionCheck(jniEnv)) {
+            (*jniEnv)->ExceptionClear(jniEnv);
+        }
+
+        if ((*jniEnv)->PushLocalFrame(jniEnv, 16) == JNI_OK) {
+#define _JNI_CHECK(x) if (!(x) || (*jniEnv)->ExceptionCheck(jniEnv)) goto cleanup
+            jclass activityClass = (*jniEnv)->GetObjectClass(jniEnv, activity->clazz);
+            _JNI_CHECK(activityClass);
+            jmethodID getResourcesMethod = (*jniEnv)->GetMethodID(jniEnv, activityClass,
+                "getResources", "()Landroid/content/res/Resources;");
+            _JNI_CHECK(getResourcesMethod);
+            jobject resources = (*jniEnv)->CallObjectMethod(jniEnv, activity->clazz,
+                getResourcesMethod);
+            _JNI_CHECK(resources);
+            jclass resourcesClass = (*jniEnv)->GetObjectClass(jniEnv, resources);
+            _JNI_CHECK(resourcesClass);
+            jmethodID getConfigurationMethod = (*jniEnv)->GetMethodID(jniEnv, resourcesClass,
+                "getConfiguration", "()Landroid/content/res/Configuration;");
+            _JNI_CHECK(getConfigurationMethod);
+            jobject configuration = (*jniEnv)->CallObjectMethod(jniEnv, resources,
+                getConfigurationMethod);
+            _JNI_CHECK(configuration);
+            jclass configurationClass = (*jniEnv)->GetObjectClass(jniEnv, configuration);
+            _JNI_CHECK(configurationClass);
+            jfieldID localeField = (*jniEnv)->GetFieldID(jniEnv, configurationClass, "locale",
+                "Ljava/util/Locale;");
+            _JNI_CHECK(localeField);
+            jobject localeObject = (*jniEnv)->GetObjectField(jniEnv, configuration, localeField);
+            _JNI_CHECK(localeObject);
+            jclass localeClass = (*jniEnv)->GetObjectClass(jniEnv, localeObject);
+            _JNI_CHECK(localeClass);
+            jmethodID toStringMethod = (*jniEnv)->GetMethodID(jniEnv, localeClass, "toString",
+                "()Ljava/lang/String;");
+            _JNI_CHECK(toStringMethod);
+            jstring valueString = (*jniEnv)->CallObjectMethod(jniEnv, localeObject, toStringMethod);
+            _JNI_CHECK(valueString);
+
+            const char *nativeString = (*jniEnv)->GetStringUTFChars(jniEnv, valueString, 0);
+            if (nativeString) {
+                result = 0;
+                strncpy(locale, nativeString, locale_max);
+                locale[locale_max - 1] = 0;
+                (*jniEnv)->ReleaseStringUTFChars(jniEnv, valueString, nativeString);
+            }
+cleanup:
+#undef _JNI_CHECK
+            if ((*jniEnv)->ExceptionCheck(jniEnv)) {
+                (*jniEnv)->ExceptionClear(jniEnv);
+            }
+            (*jniEnv)->PopLocalFrame(jniEnv, NULL);
+        }
+    }
+#else
+#error Unsupported platform
+#endif
+    if (result == 0) {
+        // Convert underscore to dash ("en_US" to "en-US")
+        // Remove encoding ("en-US.UTF-8" to "en-US")
+        char *ch = locale;
+        while (*ch != 0) {
+            if (*ch == '_') {
+                *ch = '-';
+            } else if (*ch == '.') {
+                *ch = 0;
+                break;
+            }
+            ch++;
+        }
+    } else {
+        locale[0] = 0;
+    }
+    return result;
+}
+
 /* MARK: Windows */
 
 #if defined(_WIN32)
-
-static inline unsigned int sleep(unsigned int seconds) {
-    Sleep(seconds * 1000);
-    return 0;
-}
-
-static inline int usleep(unsigned long useconds) {
-    if (useconds >= 1000000) {
-        errno = EINVAL;
-        return -1;
-    } else {
-        Sleep(useconds / 1000);
-        return 0;
-    }
-}
 
 static inline FILE *_fc_windows_fopen(const char *filename, const char *mode) {
     FILE *file = NULL;
@@ -174,7 +366,6 @@ static inline int _fc_windows_fclose(FILE *stream) {
 
 #define fopen(filename, mode) _fc_windows_fopen(filename, mode)
 #define fclose(file) _fc_windows_fclose(file)
-#define chdir(dirname) _chdir(dirname)
 
 #if defined(_DEBUG)
 
@@ -221,7 +412,30 @@ FILE* funopen(const void* __cookie,
 #error FILE_COMPAT_ANDROID_ACTIVITY must be defined as a reference to an ANativeActivity (or NULL).
 #endif
 
-#include <android/log.h>
+static pthread_key_t _fc_jnienv_key;
+static pthread_once_t _fc_jnienv_key_once = PTHREAD_ONCE_INIT;
+
+static void _fc_jnienv_detach(void *value) {
+    if (value) {
+        JavaVM *vm = (JavaVM *)value;
+        (*vm)->DetachCurrentThread(vm);
+    }
+}
+
+static void _fc_create_jnienv_key() {
+    pthread_key_create(&_fc_jnienv_key, _fc_jnienv_detach);
+}
+
+static JNIEnv *_fc_jnienv(JavaVM *vm) {
+    JNIEnv *jniEnv = NULL;
+    if ((*vm)->GetEnv(vm, (void **)&jniEnv, JNI_VERSION_1_4) != JNI_OK) {
+        if ((*vm)->AttachCurrentThread(vm, &jniEnv, NULL) == JNI_OK) {
+            pthread_once(&_fc_jnienv_key_once, _fc_create_jnienv_key);
+            pthread_setspecific(_fc_jnienv_key, vm);
+        }
+    }
+    return jniEnv;
+}
 
 static int _fc_android_read(void *cookie, char *buf, int size) {
     return AAsset_read((AAsset *)cookie, buf, (size_t)size);
