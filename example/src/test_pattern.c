6be7153c508458019f35be4da38d74499d1dcfd6
David Brackeen <brackeen@gmail.com>
2020-04-20 17:04:24 -0700

    Add Metal support to test_pattern example

--- example/src/test_pattern.c
+++ example/src/test_pattern.c
@@ -3,23 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include "glfm.h"
-#define FILE_COMPAT_ANDROID_ACTIVITY glfmAndroidGetActivity()
-#include "file_compat.h"
+#include "test_pattern_renderer.h"
 
 typedef struct {
-    GLuint textureId;
-    GLuint textureProgram;
-    GLuint textureVertexBuffer;
+    Renderer *renderer;
+    Texture texture;
 } TestPatternApp;
 
-static GLuint createTestPatternTexture(GLFMDisplay *display, uint32_t width, uint32_t height) {
+static Texture createTestPatternTexture(GLFMDisplay *display, uint32_t width, uint32_t height) {
     double top, right, bottom, left;
     glfmGetDisplayChromeInsets(display, &top, &right, &bottom, &left);
 
     static const uint32_t borderColor = 0xff0000ff;
     static const uint32_t insetColor = 0xff00ffff;
 
-    GLuint textureId = 0;
+    TestPatternApp *app = glfmGetUserData(display);
+    Texture texture = 0;
     uint32_t *data = malloc(width * height * sizeof(uint32_t));
     if (data) {
         uint32_t *out = data;
@@ -52,165 +51,78 @@ static GLuint createTestPatternTexture(GLFMDisplay *display, uint32_t width, uin
             *out++ = borderColor;
         }
 
-        glGenTextures(1, &textureId);
-        glBindTexture(GL_TEXTURE_2D, textureId);
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        texture = app->renderer->textureUpload(app->renderer, width, height, (uint8_t *)data);
 
         free(data);
     }
-    if (textureId != 0) {
+    if (texture != 0) {
         printf("Created test pattern %ix%i with insets %i, %i, %i, %i\n", width, height,
                (int)top, (int)right, (int)bottom, (int)left);
     }
-    return textureId;
+    return texture;
 }
 
 static void onSurfaceCreated(GLFMDisplay *display, int width, int height) {
-    glViewport(0, 0, width, height);
-
     TestPatternApp *app = glfmGetUserData(display);
-    if (app->textureId != 0) {
-        glDeleteTextures(1, &app->textureId);
+#if defined(__APPLE__)
+    if (glfmGetRenderingAPI(display) == GLFMRenderingAPIMetal) {
+        app->renderer = createRendererMetal(glfmGetMetalView(display));
+        printf("Hello from Metal!\n");
     }
-    app->textureId = createTestPatternTexture(display, width, height);
+#endif
+    if (!app->renderer) {
+        app->renderer = createRendererGLES2();
+        printf("Hello from GLES2!\n");
+    }
+    app->texture = createTestPatternTexture(display, width, height);
+}
+
+static void onSurfaceResized(GLFMDisplay *display, int width, int height) {
+    TestPatternApp *app = glfmGetUserData(display);
+    app->renderer->textureDestroy(app->renderer, app->texture);
+    app->texture = createTestPatternTexture(display, width, height);
 }
 
 static void onSurfaceDestroyed(GLFMDisplay *display) {
     // When the surface is destroyed, all existing GL resources are no longer valid.
     TestPatternApp *app = glfmGetUserData(display);
-    app->textureId = 0;
-    app->textureProgram = 0;
-    app->textureVertexBuffer = 0;
-}
-
-static GLuint compileShader(GLenum type, const char *shaderName) {
-    char fullPath[PATH_MAX];
-    fc_resdir(fullPath, sizeof(fullPath));
-    strncat(fullPath, shaderName, sizeof(fullPath) - strlen(fullPath) - 1);
-
-    // Get shader string
-    char *shaderString = NULL;
-    FILE *shaderFile = fopen(fullPath, "rb");
-    if (shaderFile) {
-        fseek(shaderFile, 0, SEEK_END);
-        size_t length = (size_t)ftell(shaderFile);
-        fseek(shaderFile, 0, SEEK_SET);
-
-        shaderString = malloc(length + 1);
-        if (shaderString) {
-            fread(shaderString, length, 1, shaderFile);
-            shaderString[length] = 0;
-        }
-        fclose(shaderFile);
-    }
-    if (!shaderString) {
-        printf("Couldn't read file: %s\n", fullPath);
-        return 0;
-    }
-
-    // Compile
-    const char *constChaderString = shaderString;
-    GLuint shader = glCreateShader(type);
-    glShaderSource(shader, 1, &constChaderString, NULL);
-    glCompileShader(shader);
-    free(shaderString);
-
-    // Check compile status
-    GLint status;
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
-    if (status == 0) {
-        printf("Couldn't compile shader: %s\n", shaderName);
-        GLint logLength;
-        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logLength);
-        if (logLength > 0) {
-            GLchar *log = malloc((size_t)logLength);
-            glGetShaderInfoLog(shader, logLength, &logLength, log);
-            if (log[0] != 0) {
-                printf("Shader log: %s\n", log);
-            }
-            free(log);
-        }
-        glDeleteShader(shader);
-        shader = 0;
-    }
-    return shader;
+    app->renderer->textureDestroy(app->renderer, app->texture);
+    app->renderer->destroy(app->renderer);
+    app->renderer = NULL;
 }
 
 static void onFrame(GLFMDisplay *display, double frameTime) {
     TestPatternApp *app = glfmGetUserData(display);
-
-    if (app->textureId == 0) {
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        return;
-    }
-
-    if (app->textureProgram == 0) {
-        GLuint vertShader = compileShader(GL_VERTEX_SHADER, "texture.vert");
-        GLuint fragShader = compileShader(GL_FRAGMENT_SHADER, "texture.frag");
-        if (vertShader == 0 || fragShader == 0) {
-            glfmSetMainLoopFunc(display, NULL);
-            return;
-        }
-
-        app->textureProgram = glCreateProgram();
-
-        glAttachShader(app->textureProgram, vertShader);
-        glAttachShader(app->textureProgram, fragShader);
-
-        glBindAttribLocation(app->textureProgram, 0, "position");
-        glBindAttribLocation(app->textureProgram, 1, "texCoord");
-
-        glLinkProgram(app->textureProgram);
-
-        glDeleteShader(vertShader);
-        glDeleteShader(fragShader);
-    }
-    glUseProgram(app->textureProgram);
-    if (app->textureVertexBuffer == 0) {
-        glGenBuffers(1, &app->textureVertexBuffer);
-    }
-    glBindBuffer(GL_ARRAY_BUFFER, app->textureVertexBuffer);
-    const size_t stride = sizeof(GLfloat) * 4;
-    const size_t textureCoordsOffset = sizeof(GLfloat) * 2;
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, stride, (void *)0);
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, (void *)textureCoordsOffset);
-
-    const GLfloat vertices[] = {
-        // viewX, viewY, textureX, textureY
-        -1, -1, 0, 0,
-         1, -1, 1, 0,
-        -1,  1, 0, 1,
-         1,  1, 1, 1,
+    
+    int width, height;
+    glfmGetDisplaySize(display, &width, &height);
+    app->renderer->drawFrameStart(app->renderer, width, height);
+    
+    const Vertex vertices[4] = {
+        { .position = { -1, -1 }, .texCoord = { 0, 0 } },
+        { .position = {  1, -1 }, .texCoord = { 1, 0 } },
+        { .position = { -1,  1 }, .texCoord = { 0, 1 } },
+        { .position = {  1,  1 }, .texCoord = { 1, 1 } },
     };
-
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
-    glBindTexture(GL_TEXTURE_2D, app->textureId);
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    
+    app->renderer->drawQuad(app->renderer, app->texture, &vertices);
+    app->renderer->drawFrameEnd(app->renderer);
 }
 
 void glfmMain(GLFMDisplay *display) {
     TestPatternApp *app = calloc(1, sizeof(TestPatternApp));
 
+    GLFMRenderingAPI renderingAPI = glfmIsMetalSupported(display) ? GLFMRenderingAPIMetal : GLFMRenderingAPIOpenGLES2;
     glfmSetDisplayConfig(display,
-                         GLFMRenderingAPIOpenGLES2,
+                         renderingAPI,
                          GLFMColorFormatRGBA8888,
                          GLFMDepthFormatNone,
                          GLFMStencilFormatNone,
                          GLFMMultisampleNone);
 
-    //glfmSetDisplayChrome(display, GLFMUserInterfaceChromeFullscreen);
-
     glfmSetUserData(display, app);
     glfmSetSurfaceCreatedFunc(display, onSurfaceCreated);
-    glfmSetSurfaceResizedFunc(display, onSurfaceCreated);
+    glfmSetSurfaceResizedFunc(display, onSurfaceResized);
     glfmSetSurfaceDestroyedFunc(display, onSurfaceDestroyed);
     glfmSetMainLoopFunc(display, onFrame);
 }
