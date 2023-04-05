7505eac8800677b23082f922f7d3d1a7d93632da
David Brackeen <brackeen@gmail.com>
2023-04-05 08:58:40 -0700

    Build examples for all apple platforms

--- tests/build_apple_examples.sh
+++ tests/build_apple_examples.sh
@@ -5,11 +5,17 @@ if ! type xcodebuild &> /dev/null; then
 fi
 
 export CFLAGS=-Werror=deprecated-declarations
+declare -a sdks=("appletvos" "iphoneos" "macosx")
 
 rm -Rf build/apple_examples
+
 cmake -S .. -B build/apple_examples -G Xcode \
     -D GLFM_BUILD_EXAMPLES=ON \
     -D CMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY="" \
     -D CMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_REQUIRED=NO || exit $?
-cmake --build build/apple_examples
+
+for sdk in "${sdks[@]}"; do
+    echo ">>> Building examples for $sdk"
+    cmake --build build/apple_examples -- -sdk ${sdk} || exit $?
+done
 
